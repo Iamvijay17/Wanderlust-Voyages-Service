4f05c0539f077ab5b7a2fbe8a20f513bb6b1c975
Vijay <100522623+Iamvijay17@users.noreply.github.com>
2024-12-19 10:56:21 +0530

    Update swagger.js

--- swagger.js
+++ swagger.js
@@ -1,10 +1,15 @@
 import fs from "fs";
+import path from "path";
 
 const loadJson = () => {
+  // Adjust the path to ensure it points to the correct location of swagger.json
+  const jsonPath = path.resolve(process.cwd(), "swagger.json");
 
-  
+  // Log the resolved path for debugging
+  console.log("Resolved JSON Path:", jsonPath);
+
   try {
-    const file = fs.readFileSync("./swagger.json", "utf8"); // Adjust path as needed
+    const file = fs.readFileSync(jsonPath, "utf8");
     return JSON.parse(file);
   } catch (error) {
     console.error("Error loading Swagger JSON file:", error.message);
